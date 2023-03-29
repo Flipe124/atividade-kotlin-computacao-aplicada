cd51c049e037c1e0466261b2a19ca80a48110b06
Felipe <flipeoliveira007@gmail.com>
2023-03-29 20:58:21 -0300

    Adicionando: função de comportamento

---
+++ src/main/kotlin/Two-Fer/comportamento/TwoFer.kt
@@ -0,0 +1,26 @@
+package `Two-Fer`.comportamento
+
+fun TwoFer(nome : String): String {
+    var frase = "";
+
+    if (nome == "Alice"){
+        frase = "Um para Alice, um para mim.";
+        return frase;
+
+    } else if(nome == "bob") {
+        frase = "Um para Bob, um para mim.";
+        return frase;
+
+    } else if(nome == "Jose"){
+        frase = "Um para José, um para mim.";
+        return frase;
+
+    } else if (nome == ""){
+        frase = "Um para você, um para mim.";
+        return frase;
+
+    } else {
+        frase = ";/";
+        return frase;
+    }
+}
