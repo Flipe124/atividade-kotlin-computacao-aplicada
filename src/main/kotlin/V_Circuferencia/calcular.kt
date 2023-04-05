904afa7b8d730e2f0a79414bad62d81c35e6473b
Felipe <flipeoliveira007@gmail.com>
2023-04-05 20:48:07 -0300

    Adicionando: função para calcular a circunferência

---
+++ src/main/kotlin/V_Circuferencia/calcular.kt
@@ -0,0 +1,15 @@
+package V_Circuferencia
+
+import java.util.*
+
+private var scanner = Scanner(System.`in`);
+
+fun calcular(raio : Double) : Double {
+    var pi = 3.14159265;
+    var raio = raio;
+    var resultado = 0.0;
+
+    resultado = (raio * 2) * pi;
+
+    return resultado;
+}
