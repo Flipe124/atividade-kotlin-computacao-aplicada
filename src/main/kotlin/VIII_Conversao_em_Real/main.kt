b9215e49b5dba25debf00aeff3ea5cd1935f7e58
Felipe <flipeoliveira007@gmail.com>
2023-04-19 22:30:30 -0300

    Adicionando: função de conversão para reais

---
+++ src/main/kotlin/VIII_Conversao_em_Real/main.kt
@@ -0,0 +1,20 @@
+package VIII_Conversao_em_Real
+import java.util.*;
+
+private var scanner = Scanner(System. `in`);
+
+fun main() {
+    var cotacao = 0.0;
+    var dolares = 0.0;
+    var valor_real = 0.0;
+
+    println("Informe a contação do dolar: ");
+    cotacao = scanner.nextDouble();
+
+    println("Informe a quantidade de dolares: ")
+    dolares = scanner.nextDouble();
+
+    valor_real = dolares * cotacao;
+
+    println("Quandidade em reais: R$" + valor_real);
+}
