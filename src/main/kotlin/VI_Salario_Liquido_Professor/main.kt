038862651888ac95f9d48a586b73776fc3aef9ad
Felipe <flipeoliveira007@gmail.com>
2023-04-19 21:38:40 -0300

    Adicionando: função main

---
+++ src/main/kotlin/VI_Salario_Liquido_Professor/main.kt
@@ -0,0 +1,23 @@
+package VI_Salario_Liquido_Professor
+
+import java.util.*
+
+private var scanner = Scanner(System. `in`);
+
+fun main() {
+    var valor_hora = 0.0;
+    var hora_trabalhada = 0.0;
+    var desconto = 0.0;
+
+    println("Informe o valor das horas:");
+    valor_hora = scanner.nextDouble();
+
+    println("Informe a quantidade de horas trabalhadas no mês:");
+    hora_trabalhada = scanner.nextDouble();
+
+    println("Informe o % de desconto do INSS:");
+    desconto = scanner.nextDouble();
+
+    println(calcular(valor_hora, hora_trabalhada, desconto));
+
+}
