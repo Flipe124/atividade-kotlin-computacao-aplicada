bf4bd35bd4bce8b508b74f09f7ea935160862ba7
Felipe <flipeoliveira007@gmail.com>
2023-04-19 23:40:07 -0300

    Adicionando: função main da soma dos quadrados

---
+++ src/main/kotlin/X_Soma_dos_Quadrados/main.kt
@@ -0,0 +1,24 @@
+package X_Soma_dos_Quadrados
+
+import java.util.*;
+private var scanner = Scanner(System. `in`);
+
+fun main() {
+    var quadrado1 = 0;
+    var quadrado2 = 0;
+    var quadrado3 = 0;
+    var soma = 0;
+
+    println("Informe o 1° valor: ");
+    quadrado1 = scanner.nextInt();
+
+    println("Informe o 2° valor: ");
+    quadrado2 = scanner.nextInt();
+
+    println("Informe o 3° valor: ");
+    quadrado3 = scanner.nextInt();
+
+    soma = (quadrado1*quadrado1) + (quadrado2*quadrado2) + (quadrado3*quadrado3);
+
+    println("Soma dos quadrados: " + soma);
+}
