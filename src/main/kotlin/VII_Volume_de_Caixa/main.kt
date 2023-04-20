9a18ab3b59b069beacd6477c7fc63afc66b16d41
Felipe <flipeoliveira007@gmail.com>
2023-04-19 22:08:19 -0300

    Adicionando: função main para o calculo de volume de caixa

---
+++ src/main/kotlin/VII_Volume_de_Caixa/main.kt
@@ -0,0 +1,26 @@
+package VII_Volume_de_Caixa
+
+import java.util.*;
+
+private var scanner = Scanner(System. `in`)
+
+fun main() {
+    var comprimento = 0.0;
+    var largura = 0.0;
+    var altura = 0.0;
+
+    var volume = 0.0;
+
+    println("Informe o comprimento do retângulo: ");
+    comprimento = scanner.nextDouble();
+
+    println("Informe a largura do retângulo: ");
+    largura = scanner.nextDouble();
+
+    println("Informe a altura do retângulo: ");
+    altura = scanner.nextDouble();
+
+    volume = comprimento * largura * altura;
+
+    println("Volume do retângulo: " + volume);
+}
